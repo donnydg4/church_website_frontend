3f6ba57427c08ae95a2c700baae2df4661a9e923
Donny Groezinger <donnygroezinger@gmail.com>
2022-11-29 22:31:22 -0600

    add calendar and other style changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'calendar',
+    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
+  },
 ];
 
 @NgModule({
