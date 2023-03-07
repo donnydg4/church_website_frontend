a179d65c0dbab9e15b9e7e73904723d3ffc0b45f
Donny Groezinger <donnygroezinger@gmail.com>
2023-03-07 15:47:48 -0600

    update changes

--- src/app/all-events/event/event.component.ts
+++ src/app/all-events/event/event.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {MainEventModel} from "../../models/main-event-model";
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: 'app-event',
@@ -11,7 +12,15 @@ export class EventComponent implements OnInit {
 
   @Input() eventsArray: Observable<MainEventModel[]>
 
-  constructor() { }
+  public platformWidth = this.platform.width()
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.platformWidth = this.platform.width();
+    console.log(this.platformWidth);
+  }
+
+  constructor(private platform: Platform) { }
 
   ngOnInit() {}
 
