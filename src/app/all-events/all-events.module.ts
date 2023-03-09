a454ab977986c9d198d1fce8bd3cc546cdbad549
Donny Groezinger <donnygroezinger@gmail.com>
2023-03-09 13:13:53 -0600

    change events page

--- src/app/all-events/all-events.module.ts
+++ src/app/all-events/all-events.module.ts
@@ -8,14 +8,16 @@ import { AllEventsPageRoutingModule } from './all-events-routing.module';
 
 import { AllEventsPage } from './all-events.page';
 import {EventComponent} from "./event/event.component";
+import {SharedComponentsModule} from "../shared-components/shared-components.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AllEventsPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        AllEventsPageRoutingModule,
+        SharedComponentsModule
+    ],
     declarations: [AllEventsPage, EventComponent]
 })
 export class AllEventsPageModule {}
