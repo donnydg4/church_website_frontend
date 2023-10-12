8eb8d309cdd6fb1f5d7c91f6d274d11aef5841b0
Donny Groezinger <donnygroezinger@gmail.com>
2023-10-12 10:26:44 -0500

    add app header to all pages + set min/max height for pictures for displayCards

--- src/app/our-ministries/our-ministries.module.ts
+++ src/app/our-ministries/our-ministries.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,6 +17,7 @@ import {SharedComponentsModule} from "../shared-components/shared-components.mod
     OurMinistriesPageRoutingModule,
     SharedComponentsModule
   ],
-  declarations: [OurMinistriesPage]
+  declarations: [OurMinistriesPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class OurMinistriesPageModule {}
