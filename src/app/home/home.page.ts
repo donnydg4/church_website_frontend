df31664c8dd976717310d8d9f91735d8bad6adc4
Donny Groezinger <donnygroezinger@gmail.com>
2023-12-11 21:58:52 -0600

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {NavController} from "@ionic/angular";
 import {AllChurchInformationService} from "../service/all-church-information.service";
 import {map, tap} from "rxjs/operators";
 import {sortByDateEvents} from "../utils/utils";
+import {OurMinistriesModel} from "../models/sub-models/our-ministries.model";
+import {MinistriesWeSupportModel} from "../models/sub-models/ministries-we-support.model";
+import {BusinessesWeSupportModel} from "../models/sub-models/businesses-we-support.model";
+import {MissionsModel} from "../models/sub-models/missions.model";
+import {MainEventModel} from "../models/sub-models/main-event.model";
+import {WaysToEngageModel} from "../models/sub-models/ways-to-engage.model";
 
 @Component({
   selector: 'app-home',
@@ -14,14 +20,21 @@ export class HomePage {
   constructor(private navCtrl: NavController, private dataService: AllChurchInformationService) {
   }
 
+  waysToEngage = signal<WaysToEngageModel[]>([])
+
   homeInformation$ = this.dataService.allWebsiteInformation$
     .pipe(
+      tap(data => {
+        this.waysToEngage.set([
+          {type: '/missions', imageUrl: `${data.missionsPage.coverPhoto}`, description: 'MISSIONS'},
+          {type: '/our-ministries', imageUrl: `${data.ourMinistriesPage.coverPhoto}`, description: 'OUR MINISTRIES'},
+          {type: '/sermons', imageUrl: `${data.ourMinistriesPage.coverPhoto}`, description: 'MESSAGES'},
+          {type: '/events', imageUrl: `${data.mainEvents.coverPhoto}`, description: 'ALL EVENTS'},
+          {type: '/connect', imageUrl: `${data.mainEvents.coverPhoto}`, description: 'CONTACT US'},
+          {type: '/our-church', imageUrl: `${data.leadershipPage.coverPhoto}`, description: 'OUR CHURCH'},
+        ])
+      }),
       map(data => data.allWebsiteInformation.homePage)
-    );
-
-  getAllEvents$ = this.dataService.allWebsiteInformation$
-    .pipe(
-      map(events => events.mainEvents.events.filter(event => event.type === 'event').sort(sortByDateEvents))
     );
 
   navigateToPage(type: string) {
---
+++ src/app/models/sub-models/ways-to-engage.model.ts
@@ -0,0 +1,5 @@
+export interface WaysToEngageModel {
+  type: string;
+  imageUrl: string;
+  description: string;
+}
