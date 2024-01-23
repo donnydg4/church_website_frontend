17bfb5e5ac6e1ebef754ca450993eaaaea9ff99c
Donny Groezinger <donnygroezinger@gmail.com>
2024-01-23 14:15:40 -0600

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -121,7 +121,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, anchorScrolling: 'enabled'})
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
