250de99775a2edc8473867e13d6f52b85d034567
Donny Groezinger <donnygroezinger@gmail.com>
2024-02-08 14:52:29 -0600

    updated stuff

---
+++ src/app/models/sub-models/about.model.ts
@@ -0,0 +1,7 @@
+export interface About {
+  crazyForJesus: string[];
+  missionVisionTargetTitles: string[];
+  missionVisionTargetDescriptions: string[];
+  coreValues: string[];
+  declarations: string[];
+}
