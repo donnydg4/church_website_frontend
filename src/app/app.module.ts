f1197fa1005f2b8185e68aed071cd8b7b3cfa00c
Donny Groezinger <donnygroezinger@gmail.com>
2024-05-23 14:08:03 -0500

    add back service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,12 +56,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     NgxPaginationModule,
     HttpClientModule,
     MatSnackBarModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: !isDevMode(),
-    //   // Register the ServiceWorker as soon as the application is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
