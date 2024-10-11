983ecaa76628174faa93024866e8c56582ebb265
Donny Groezinger <donnygroezinger@gmail.com>
2024-10-11 15:34:59 -0500

    almost finish worship music video stuf

--- src/app/worship/worship.page.ts
+++ src/app/worship/worship.page.ts
@@ -1,6 +1,7 @@
 import {Component, computed, inject, OnInit} from '@angular/core';
 import {NavController} from "@ionic/angular";
 import {AllChurchInformationService} from "../service/all-church-information.service";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-worship',
@@ -11,12 +12,23 @@ export class WorshipPage {
 
   private dataService = inject(AllChurchInformationService);
   private navCtrl = inject(NavController);
+  private sanitizer = inject(DomSanitizer);
 
   homeInfo = computed(() => this.dataService.allChurchInformation().allWebsiteInformation?.homePage);
 
-  fakeArray: string[] = ['', '', '',''];
+  fakeArray: string[] = ['5xQEgz7J3RA', 'Xk6LKuqj3Xc', 'qv3-TDdD1pM','-h9wpvwAF-M'];
+  defaultUrl: string = "https://www.youtube.com/embed/";
+  youtubeUrl: SafeResourceUrl;
+
+  constructor() {
+    this.youtubeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.defaultUrl + this.fakeArray[0]);
+  }
 
   navigateToPage(type: string) {
     this.navCtrl.navigateForward([type]);
+  }
+
+  openVideoModal(id: number){
+    console.log(this.youtubeUrl);
   }
 }
