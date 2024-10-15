387051189d4e0bb024a640eb8ab2aa6c22415e7d
Donny Groezinger <donnygroezinger@gmail.com>
2024-10-14 21:15:59 -0500

    audio changes

--- src/app/worship/worship.module.ts
+++ src/app/worship/worship.module.ts
@@ -8,16 +8,19 @@ import { WorshipPageRoutingModule } from './worship-routing.module';
 
 import { WorshipPage } from './worship.page';
 import {SharedComponentsModule} from "../shared-components/shared-components.module";
+import {MatSlider, MatSliderThumb} from "@angular/material/slider";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        WorshipPageRoutingModule,
-        NgOptimizedImage,
-        SharedComponentsModule
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    WorshipPageRoutingModule,
+    NgOptimizedImage,
+    SharedComponentsModule,
+    MatSlider,
+    MatSliderThumb
+  ],
   declarations: [WorshipPage]
 })
 export class WorshipPageModule {}
