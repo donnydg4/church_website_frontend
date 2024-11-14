cbf0624dad52a01fbb15446c68b3d5d098b25625
Donny Groezinger <donnygroezinger@gmail.com>
2024-11-14 15:24:58 -0600

    trial

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/church_website_frontend'));
+app.use(express.static(__dirname + '/dist/app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/church_website_frontend/browser/index.html'));});
+    '/dist/app/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
