11377d2abba1a328d44d895922690d254e98df34
Donny Groezinger <donnygroezinger@gmail.com>
2024-11-14 22:27:49 -0600

    chagnes'

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/church_website_frontend/browser'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/browser/church_website_frontend/browser/index.html'));});
+    '/dist/church_website_frontend/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
