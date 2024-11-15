58d7a3aa55fb73a78acd4bddeff7ed051e974326
Donny Groezinger <donnygroezinger@gmail.com>
2024-11-14 22:25:52 -0600

    cahnges

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('dist/browser/church_website_frontend'));
+app.use(express.static(__dirname + '/dist/church_website_frontend/browser'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join('dist/browser/church_website_frontend/index.html'));});
+  res.sendFile(path.join(__dirname+
+    '/dist/browser/church_website_frontend/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
