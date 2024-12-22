9fdab518976301d56bf25f28fca87b02e777ecaf
Donny Groezinger <donnygroezinger@gmail.com>
2024-12-21 20:40:18 -0600

    add sozo stuff

--- src/app/models/sub-models/sozo.model.ts
+++ src/app/models/sub-models/sozo.model.ts
@@ -2,4 +2,11 @@ export interface SozoPageModel {
   firstImage: string;
   firstImageTitle: string;
   textBreak: string;
+  textBreakTwo: string;
+  imageOne: string;
+  imageOneText: string[];
+  imageTwo: string;
+  imageTwoText: string[];
+  imageThree: string;
+  imageThreeText: string[];
 }
--- src/app/sozo/sozo.page.ts
+++ src/app/sozo/sozo.page.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FooterComponent} from '../shared-components/footer/footer.component';
-import {IonCol, IonContent, IonGrid, IonRow, IonSpinner} from "@ionic/angular/standalone";
+import {IonCard, IonCol, IonContent, IonGrid, IonRow, IonSpinner, IonText} from "@ionic/angular/standalone";
 import {DisplayCardsComponent} from "../shared-components/display-cards/display-cards.component";
 import {NgOptimizedImage} from "@angular/common";
 import {AllChurchInformationService} from "../service/all-church-information.service";
@@ -10,15 +10,17 @@ import {AllChurchInformationService} from "../service/all-church-information.ser
   templateUrl: './sozo.page.html',
   styleUrls: ['./sozo.page.scss'],
   standalone: true,
-    imports: [
-        FooterComponent,
-        IonCol,
-        IonContent,
-        IonGrid,
-        IonRow,
-        IonSpinner,
-        NgOptimizedImage
-    ]
+  imports: [
+    FooterComponent,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonSpinner,
+    NgOptimizedImage,
+    IonCard,
+    IonText
+  ]
 })
 export class SozoPage {
 
