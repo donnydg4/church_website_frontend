94f6c36da5481aaa292d15e9c7515e9a1d95782f
Donny Groezinger <donnygroezinger@gmail.com>
2024-12-28 22:26:45 -0600

    changes

--- src/app/kids-ministry/kids-ministry.page.ts
+++ src/app/kids-ministry/kids-ministry.page.ts
@@ -1,15 +1,25 @@
-import {Component} from '@angular/core';
-import {IonCol, IonContent, IonGrid, IonRow} from "@ionic/angular/standalone";
+import {Component, HostListener, inject} from '@angular/core';
+import {IonCol, IonContent, IonGrid, IonRow, Platform} from "@ionic/angular/standalone";
+import {NgOptimizedImage} from "@angular/common";
+import {AllChurchInformationService} from "../service/all-church-information.service";
 
 @Component({
   selector: 'app-kids-ministry',
   templateUrl: './kids-ministry.page.html',
   styleUrls: ['./kids-ministry.page.scss'],
   standalone: true,
-  imports: [IonContent, IonGrid, IonRow, IonCol]
+    imports: [IonContent, IonGrid, IonRow, IonCol, NgOptimizedImage]
 })
 export class KidsMinistryPage {
+  private platform = inject(Platform);
+  private dataService = inject(AllChurchInformationService);
 
-  constructor() { }
+  public platformWidth = this.platform.width()
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.platformWidth = this.platform.width();
+  }
+
+  kidYouth = this.dataService.allWebsiteInformationTwo.value;
 }
